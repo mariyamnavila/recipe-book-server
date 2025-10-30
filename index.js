7999814807dee45cf7b0dfd52c418f7794651c0d
mariyamnavila <bibimariyamnavila@gmail.com>
2025-10-30 21:10:35 +0600

    added delete method

--- index.js
+++ index.js
@@ -79,6 +79,12 @@ async function run() {
             res.send(result);
         })
 
+        app.delete('/recipes/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await recipesCollection.deleteOne(query);
+            res.send(result);
+        })
         // Send a ping to confirm a successful connection
 
         await client.connect();
