c24b401d24ded42ead0197a333a3e7c0f94c2ee4
mariyamnavila <bibimariyamnavila@gmail.com>
2025-10-31 13:22:03 +0600

    added insertone method for liked recipes

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ async function run() {
             const updatedRecipe = req.body;
             const options = { upsert: true };
             const updateDoc = {
-                $set: { updatedRecipe },
+                $set:  updatedRecipe ,
             };
             const result = await recipesCollection.updateOne(filter, updateDoc, options);
             res.send(result);
@@ -87,8 +87,8 @@ async function run() {
         })
         // Send a ping to confirm a successful connection
 
-        await client.connect();
-        await client.db("admin").command({ ping: 1 });
+        // await client.connect();
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // await client.close();
